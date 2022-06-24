b2198f4d6d34bfaa5fcb704a6b0879e053306e65
Rickyesk <74616797+Rickyesk@users.noreply.github.com>
2022-06-24 11:39:46 +0100

    Delete OLAP.sql

--- OLAP.sql
+++
@@ -1,8 +0,0 @@
-1. num dado período (i.e. entre duas datas), por dia da semana, por concelho e no total
-
-	SELECT dia_semana, conc, SUM(unid) FROM (SELECT * FROM vendas WHERE instante BETWEEN *input1* AND *input2*) GROUP BY ROLLUP(dia_semana, conc)
-
-
-2. num dado distrito (i.e. “Lisboa”), por concelho, categoria, dia da semana e no total
-
-	SELECT conc, categ, dia_semana, COUNT(dist) FROM vendas GROUP BY ROLLUP(conc, categ, dia_semana)
